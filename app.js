a8fc3b144b2919ef8adcd3c4be79dc92b81348bb
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2022-12-06 21:05:00 -0300

    pronto repetição

---
+++ app.js
@@ -0,0 +1,14 @@
+function tabuada(){
+
+    // enquantoa condição for verdadeira faça algo 
+
+    let contador=0
+   let n1 = document.getElementById("n1").value
+   document.getElementById("resposta").innerHTML=""
+
+    while(contador<=10){
+
+        document.getElementById("resposta").innerHTML +=n1 +"x"+ contador +"=" + contador*n1 + "<br>"
+        contador=contador+1
+    }
+}
